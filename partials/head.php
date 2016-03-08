69b95c513f5a95ad99382596e2ad3c23af1b1acf
DEV <dev@dunckelfeld.de>
2016-03-08 14:27:22 +0100

    created partials for favicon and meta

--- partials/head.php
+++ partials/head.php
@@ -1,26 +1,11 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="initial-scale=1.0">
-    <title><?php wp_title( '|', true, 'right' ) ?></title>
-
-    <!-- Favicons -->
-    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/assets/icons/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/assets/icons/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory'); ?>/assets/icons/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/assets/icons/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_directory'); ?>/assets/icons/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/assets/icons/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_directory'); ?>/assets/icons/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/assets/icons/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/assets/icons/apple-touch-icon-152x152.png" />
+    <?php include('partials/meta.php'); ?>
+    <?php include('partials/favicons.php'); ?>
 
     <!-- WP and Plugin Scripts and styles -->
     <?php wp_head(); ?>
-
-
 </head>
 <body <?php body_class() ?>>
-	<div class="wrapper">
+    <div class="wrapper">
---
+++ partials/meta.php
@@ -0,0 +1,4 @@
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="initial-scale=1.0">
+    <title><?php wp_title( '|', true, 'right' ) ?></title>
